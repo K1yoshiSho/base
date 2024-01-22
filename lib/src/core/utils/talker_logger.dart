c1b9c99e483a26869a845e528044bb56b550a8e5
Yelaman Yelmuratov <e99xxx@gmail.com>
2024-01-23 02:56:44 +0600

    Initialize handling

---
+++ lib/src/core/utils/talker_logger.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:talker_bloc_logger/talker_bloc_logger.dart';
+import 'package:talker_flutter/talker_flutter.dart';
+
+/// [Talker] - This class contains methods for handling errors and logging.
+
+/// `initHandling` - This function initializes handling of the app.
+
+Future<void> initHandling({required Talker talker}) async {
+  FlutterError.presentError = (details) {
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      talker.handle(details.exception, details.stack);
+    });
+  };
+
+  Bloc.observer = TalkerBlocObserver(
+    talker: talker,
+    settings: const TalkerBlocLoggerSettings(
+      printStateFullData: false,
+    ),
+  );
+
+  // PlatformDispatcher.instance.onError = (error, stack) {
+  //   if (FlavorConfig.instance.name == FlavorKey.kProduction &&
+  //       kDebugMode == false) {
+  //     FirebaseCrashlytics.instance.recordError(error, stack);
+  //   }
+  //   dependenciesContainer.talker.handle(error, stack);
+  //   return true;
+  // };
+
+  // FlutterError.onError = (details) => {
+  //       if (FlavorConfig.instance.name == FlavorKey.kProduction &&
+  //           kDebugMode == false) ...[
+  //         FirebaseCrashlytics.instance
+  //             .recordError(details.exception, details.stack),
+  //       ],
+  //      dependenciesContainer.talker.handle(details.exception, details.stack),
+  //     };
+}
